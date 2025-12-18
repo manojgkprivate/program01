6ef80950a7f8902f4034e344f9462cab734a2895
manojgkprivate <manojgkprivate@gmail.com>
2025-12-18 19:25:40 +0530

    Create App.js with basic React component

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Resume from "./Resume";
+// import "./style.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Resume />
+    </div>
+  );
+}
+
+export default App;
